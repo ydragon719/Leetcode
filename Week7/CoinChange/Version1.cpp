7038c279b16f1c4102e4ca37abb0e9ae87dfe610
ydragon719 <85536991+ydragon719@users.noreply.github.com>
2022-01-16 19:57:33 +0800

    Create Version1.cpp

---
+++ Week7/CoinChange/Version1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int len = coins.size();
+        vector<int> dp(amount+1, -1);
+        dp[0] =0;
+        
+        for(int i=1; i<amount+1; i++)
+        {
+			int min_now = INT_MAX;
+            for(int k=0; k<coins.size(); k++)
+            {
+                if(i>coins[k] && dp[i-coins[k] != -1])
+					min_now = min(min_now, dp[i-coins[k] +1]);
+            }
+			dp[i] = min_now == INT_MAX? -1:min_now;
+            
+        }
+        return dp[amount];
+    }
+};
