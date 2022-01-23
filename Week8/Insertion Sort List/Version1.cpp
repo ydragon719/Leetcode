fe10f028c938bedc9510f29961e7f83bd9c46edc
ydragon719 <85536991+ydragon719@users.noreply.github.com>
2022-01-23 18:25:25 +0800

    Create Version1.cpp

---
+++ Week8/Insertion Sort List/Version1.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if(!head)
+            return head;
+        ListNode * now;
+        ListNode * pos = head->next;
+        ListNode * end = head;
+        while(pos)
+        {
+            end->next = NULL;
+            now = pos;
+            pos = pos->next;
+            ListNode * start = head;
+            ListNode * before = NULL;
+            while(start)
+            {
+                if(now->val> start->val)
+                {  
+                    before = start;
+                    start = start->next;
+                }
+                else
+                    break;
+            }
+            if(before == NULL)
+            {
+                now->next = head;
+                head = now;
+            }
+            else
+            {
+                before->next = now;
+                if(start == NULL)
+                    end = now;
+                else
+                    now->next = start;
+            }
+            
+        }
+        return head;
+    }
+};
